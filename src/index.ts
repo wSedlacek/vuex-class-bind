47148d4da0044cb0606cf152cf4b2c87b9e7d348
wSedlacek <wsedlacekc@gmail.com>
2019-05-13 22:12:30 -0500

    Support more text formats.
    Add support for . and / on Action and State Variable.

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,39 @@
-import { createDecorator } from 'vue-class-component'
+import { createDecorator } from "vue-class-component";
 
-function getDeepValue(st:string, obj:any) {
-    return st.replace(/\[([^\]]+)]/g, '.$1').split('.').reduce(function(o, p) {
-        return o[p];
+function getDeepValue(st: string, obj: any) {
+  return st
+    .replace(/\[([^\]]+)]/g, ".$1")
+    .split(".")
+    .reduce(function(o, p) {
+      return o[p];
     }, obj);
 }
-type functionGetState = (state:any) => any
+type functionGetState = (state: any) => any;
 
-export function Bind(action: string, stateVariable?: string|functionGetState): any {
-    return createDecorator((componentOptions, k) => {
-        if(!componentOptions.computed) {
-            componentOptions.computed = {}
-        }
+export function Bind(
+  action: string,
+  stateVariable?: string | functionGetState,
+): any {
+  return createDecorator((componentOptions, k) => {
+    if (!componentOptions.computed) {
+      componentOptions.computed = {};
+    }
 
-        componentOptions.computed[k] = {
-            get() {
-                if(typeof stateVariable === "string") {
-                    return stateVariable? getDeepValue(stateVariable, (this as any).$store.state) : (this as any).$store.state[k]
-                } else if (stateVariable) {
-                    return stateVariable((this as any).$store.state)
-                }
-            },
-            set(val) {
-                (this as any).$store.dispatch(action, val)
-            }
+    componentOptions.computed[k] = {
+      get() {
+        if (typeof stateVariable === "string") {
+          const correctedStateVariable = stateVariable.replace("/", ".");
+          return correctedStateVariable
+            ? getDeepValue(correctedStateVariable, (this as any).$store.state)
+            : (this as any).$store.state[k];
+        } else if (stateVariable) {
+          return stateVariable((this as any).$store.state);
         }
-    })
+      },
+      set(val) {
+        const correctedAction = action.replace("/", ".");
+        (this as any).$store.dispatch(correctedAction, val);
+      },
+    };
+  });
 }
